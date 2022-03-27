b8d6658afa7fce88063457861890e1ca1a3074f6
Jenny Dcruz <jendcruz23@gmail.com>
2022-03-27 14:00:41 -0400

    added: functionality to display APOD for multiple days

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // Google translate
-const res = require('express/lib/response');
+const res = require("express/lib/response");
 
-const {Translate} = require('@google-cloud/translate').v2;
-require('dotenv').config();
+const { Translate } = require("@google-cloud/translate").v2;
+require("dotenv").config();
 
 // Credentials from the .env file
 const CREDENTIALS = JSON.parse(process.env.CREDENTIALS);
@@ -10,20 +10,20 @@ const CREDENTIALS = JSON.parse(process.env.CREDENTIALS);
 // Create client by passion credentials and project id
 const translate = new Translate({
   credentials: CREDENTIALS,
-  projectId: CREDENTIALS.project_id
+  projectId: CREDENTIALS.project_id,
 });
 
 // Function to translate text to given language
 async function translateText(text, targetLanguage) {
   try {
-      let [response] = await translate.translate(text, targetLanguage);
-      console.log(response);
-      return response;
+    let [response] = await translate.translate(text, targetLanguage);
+    // console.log(response);
+    return response;
   } catch (error) {
-      console.log(`Error at translateText --> ${error}`);
-      return 0;
+    console.log(`Error at translateText --> ${error}`);
+    return 0;
   }
-};
+}
 
 // ================================================================================
 // NASA's APOD
@@ -31,55 +31,84 @@ async function translateText(text, targetLanguage) {
 const exp = require("express");
 const path = require("path");
 const fs = require("fs");
-
 const port = "8888";
 const app = exp();
-
-const axios = require('axios');
-
-let key = "rS5SNNMfkBzFjw3fsx7Dog55tmoHL0czX7cu1Jo6";
-// let date = "2017-07-08";
-// let url = "https://api.nasa.gov/planetary/apod?api_key="+key+"&date="+date;
-let url = "https://api.nasa.gov/planetary/apod?api_key="+key;
+const axios = require("axios");
+const { start } = require("repl");
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
-app.use("/public", exp.static(path.join(__dirname, 'public')));
+let key = "rS5SNNMfkBzFjw3fsx7Dog55tmoHL0czX7cu1Jo6";
+
+app.use("/public", exp.static(path.join(__dirname, "public")));
 
 app.listen(port, () => {
-    console.log(`Listening on http://localhost:${port}`);
-  });
+  console.log(`Listening on http://localhost:${port}`);
+});
 
 app.get("/", (req, res) => {
-    getstuff(res);
-}); 
+  let startDate, endDate;
+  if (req.query.archiveEndDate) {
+    if (req.query.archiveStartDate) {
+      startDate = req.query.archiveStartDate;
+      endDate = req.query.archiveEndDate;
+    }
+  } 
+  else {
+    startDate = new Date().toISOString().slice(0, 10);
+    endDate = new Date().toISOString().slice(0, 10);
+  }
+  console.log(startDate);
+  console.log(endDate);
+  let url =
+    "https://api.nasa.gov/planetary/apod?api_key=" + key + "&start_date=" + startDate +"&end_date=" +endDate;
+    multipleDays(res, url);
+});
 
-// async function getAPOD() {
-//     var {data} = await axios.get(url);
-//     return data;
-// }
-
-async function getstuff(res) {
-    var pageData = {
-      title: "Popular Shows",
-      shows: null,
-    };
-    axios(
-      //the request
-      {
-        // url,
-        baseURL: url,
-        method: "get",
-        // headers,
-      }
-    ).then(async function (response){
-        console.log(response.data);
-        console.log("title is: "+response.data.title+"... in english");
-        var translation = await translateText(response.data.title, 'fr');
-        console.log("translated title is: "+translation);
-        res.render('index', response.data);
-    })
+async function multipleDays(res, url) {
+  var pageData = {
+    title: "NASA APOD API",
+  };
+  axios(
+    //the request
+    {
+      baseURL: url,
+      method: "get",
+    }
+  ).then(async function (response) {
+    const displayData = [];
+    for (var val of response.data){
+      displayData.push(val);
+    }
+    console.log(displayData);
+    res.render("index", {displayData});
+  });
 }
 
+// async function singleDay(res, url) {
+//   var pageData = {
+//     title: "NASA APOD API",
+//   };
+//   axios(
+//     //the request
+//     {
+//       baseURL: url,
+//       method: "get",
+//     }
+//   ).then(async function (response) {
+//     // console.log(response.data);
+//     // console.log("title is: " + response.data.title + "... in english");
+//     var translation = await translateText(response.data.title, "fr");
+//     // console.log("translated title is: " + translation);
 
+//     // var frDataArr = response.data;
+//     let frData = {
+//       frTitle: await translateText(response.data.title, "fr"),
+//     };
+//     // response.data.push(frData);
+//     console.log(response.data);
+//     var d
+//     res.render("index", {response.data});
+//   });
+// }
